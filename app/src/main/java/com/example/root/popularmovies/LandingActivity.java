7967b55e08af4f00046dfba2b794a84febef938d
satyaDroid <satyadroiddev@gmail.com>
2016-11-01 22:00:08 +0530

    First commit

---
+++ app/src/main/java/com/example/root/popularmovies/LandingActivity.java
@@ -0,0 +1,15 @@
+package com.example.root.popularmovies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.squareup.picasso.Picasso;
+
+public class LandingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+    }
+}
